3738f055ae836f9f54c27ece73540aa7ded6fedf
Abhishek Mahajan <amahajan338@gmail.com>
2025-08-11 22:00:05 +0530

    Initial Setup of the Maven project with few programs files.

---
+++ src/test/java/com/abhishekmahajan/sample/ex_01_RA_Basics/RestAssuredHelloWorld.java
@@ -0,0 +1,18 @@
+package com.abhishekmahajan.sample.ex_01_RA_Basics;
+
+import io.restassured.RestAssured;
+
+public class RestAssuredHelloWorld {
+    public void main (String[] args){
+        // https://restful-booker.herokuapp.com/ping
+        //
+
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when()
+                .log().all().get().
+                then()
+                .log().all().statusCode(201);
+    }
+}
